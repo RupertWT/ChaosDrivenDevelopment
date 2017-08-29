28bb6b418064e0a076c51613f8e4f1e658fbb2a4
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@lap-lin-2186.impello.co.uk>
2017-08-29 14:53:13 +0100

    ...

--- src/main/java/com/rupert/learning/ChaosDrivenDevelopmentKata/CommandsDecompactor.java
+++ src/main/java/com/rupert/learning/ChaosDrivenDevelopmentKata/CommandsDecompactor.java
@@ -3,7 +3,10 @@ package com.rupert.learning.ChaosDrivenDevelopmentKata;
 public class CommandsDecompactor {
 
 	public String[] decompact(String[] compactedCommands) {
-		String[] result = new String[] {"DF" };
+		
+		String[] result = new String[compactedCommands.length];
+		result = compactedCommands;
+	
 		return result;
 	}
 
--- src/test/java/com/rupert/learning/ChaosDrivenDevelopmentKata/CommandsDecompactorTest.java
+++ src/test/java/com/rupert/learning/ChaosDrivenDevelopmentKata/CommandsDecompactorTest.java
@@ -7,9 +7,25 @@ import org.junit.Test;
 public class CommandsDecompactorTest {
 		
 	@Test
-	public void oneCommandReturnsOneCommand() {
+	public void oneRegularCommandReturnsOneRegularCommand() {
 		CommandsDecompactor decompactor = new CommandsDecompactor();
 		String[] testCommands = new String[] {"DF"};
+		String[] expectedResult = new String[] {"DF"};
+		assertArrayEquals(expectedResult, decompactor.decompact(testCommands));
+	}
+	
+	@Test
+	public void twoRegularCommandsReturnsTwoRegularCommands() {
+		CommandsDecompactor decompactor = new CommandsDecompactor();
+		String[] testCommands = new String[] {"DF","TL"};
+		String[] expectedResult = new String[] {"DF","TL"};
+		assertArrayEquals(expectedResult, decompactor.decompact(testCommands));
+	}
+	
+	@Test
+	public void oneCompactedCommandWithIntOf1ReturnsTwoRegularCommands() {
+		CommandsDecompactor decompactor = new CommandsDecompactor();
+		String[] testCommands = new String[] {"DF 1"};
 		String[] expectedResult = new String[] {"DF"};
 		assertArrayEquals(expectedResult, decompactor.decompact(testCommands));
 	}
